a1afefdc33a06e64ae4bacc628b502d5f2aecd1d
Ryan Spring <rspring@nvidia.com>
2025-05-22 10:16:01 -0700

    Modify IdModel Loop map to be aware of stage_slice_position
    * Need to update ComputeAtMap, Allocation, and consumer indexing

--- csrc/device_lower/pass/circular_buffer.cpp
+++ csrc/device_lower/pass/circular_buffer.cpp
@@ -966,6 +966,7 @@ class CloneTmaCircularBufferLoopAndInsertSync
     // Use the if-then-else with electSync() predicate for the arrive expect
     // and cpAsyncBulk operations.
     kir::IfThenElse* if_expr = getElectSyncIfThenElse();
+    std::cout << "addTmaLoadBlock\t" << if_expr->toString() << std::endl;
 
     // Wait for WAR
     if (usesMBarrierForWAR()) {
@@ -984,6 +985,7 @@ class CloneTmaCircularBufferLoopAndInsertSync
     // Arrive expect tx for RAW
     if_expr->thenBody().push_back(mbarrier_arrive_tx_);
     if_expr->thenBody().push_back(expr);
+    std::cout << "addTmaLoadBlock\t" << if_expr->toString() << std::endl;
 
     mbarrier_arrive_tx_ = nullptr;
   }
